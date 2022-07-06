3638dd9478896e73b92290a3e462a9208403dc16
hamzat <hamzat1.siby@epitech.eu>
2022-07-06 12:37:39 +0200

    initialisation node and prisma

---
+++ back-horaires/db/migrations/20220706102958_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `fuseau` VARCHAR(191) NOT NULL,
+    `jours` VARCHAR(191) NOT NULL,
+    `horaire` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
